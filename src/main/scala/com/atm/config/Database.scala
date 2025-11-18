6b7a21778ae5941c712a8c530a65b10329e7acda
Emma3TC-main <U22329385@utp.edu.pe>
2025-11-17 23:49:55 -0500

    Fix env vars for DB config and update Dockerfile

--- src/main/scala/com/atm/config/Database.scala
+++ src/main/scala/com/atm/config/Database.scala
@@ -4,19 +4,23 @@ import cats.effect._
 import doobie._
 import doobie.hikari._
 import doobie.util.ExecutionContexts
-import com.typesafe.config.ConfigFactory
 
 object Database {
-  private val conf = ConfigFactory.load().getConfig("db")
+
+  // Tomar variables de entorno
+  private val driver   = sys.env.getOrElse("DB_DRIVER", "org.postgresql.Driver")
+  private val url      = sys.env.getOrElse("DB_URL", "")
+  private val user     = sys.env.getOrElse("DB_USER", "")
+  private val password = sys.env.getOrElse("DB_PASSWORD", "")
 
   def transactor[F[_]: Async]: Resource[F, HikariTransactor[F]] =
     for {
       ce <- ExecutionContexts.fixedThreadPool(32)
       xa <- HikariTransactor.newHikariTransactor[F](
-        conf.getString("driver"),
-        conf.getString("url"),
-        conf.getString("user"),
-        conf.getString("password"),
+        driver,
+        url,
+        user,
+        password,
         ce
       )
     } yield xa
